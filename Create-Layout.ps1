fda1ccd4566324a84b218cb2916c4be83cf71802
luka97 <32963849+luka97@users.noreply.github.com>
2022-12-16 11:23:45 +0100

    Updated Create-Layout.ps1

--- Create-Layout.ps1
+++ Create-Layout.ps1
@@ -11,6 +11,9 @@ $json = @{
 # Resolve the relative path of all files
 $files = Resolve-Path -Relative $files
 
+# Stores the sum of the size of all files in the project
+$total_file_size = 0
+
 foreach ($file in $files) {
     # Create the correct structure
     $entry = @{
@@ -19,9 +22,16 @@ foreach ($file in $files) {
         "date" = (Get-ItemProperty $file).LastWriteTime.ToFileTime()
     }
     $json["content"] += $entry
+    $total_file_size += (Get-ItemProperty $file).Length
 }
 
 # Convert to JSON and output in layout.json
 $json | ConvertTo-Json > layout.json
+
+if (Test-Path "./manifest.json") {
+    $manifest = (Get-Content "manifest.json" | ConvertFrom-Json)
+    $manifest.total_package_size = $total_file_size.ToString().PadLeft(20,'0')
+    $manifest | ConvertTo-Json > manifest.json
+}
 
 Set-Location ..
